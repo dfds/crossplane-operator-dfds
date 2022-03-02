a0469e64164554e31d2b165af38b936beecb8179
Richard Fisher <rifis@dfds.com>
2022-03-02 11:25:58 +0000

    WIP. Add ability to set annotation names to use

--- controllers/namespace_controller.go
+++ controllers/namespace_controller.go
@@ -18,6 +18,7 @@ package controllers
 
 import (
 	"context"
+	"os"
 
 	provideraws "github.com/crossplane/provider-aws/apis/v1beta1"
 	corev1 "k8s.io/api/core/v1"
@@ -29,11 +30,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-)
-
-//TODO: Read value(s) from configmap or other source
-const (
-	namespaceCapabilityNameLabel = "capability-name"
 )
 
 // NamespaceReconciler reconciles a Namespace object
@@ -79,8 +75,30 @@ func (r *NamespaceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 		return ctrl.Result{}, err
 	}
 
+	log.Log.Info("Processing namespace " + namespace.Name)
+
 	// Detect annotation
-	labelIsPresent := len(namespace.Annotations[namespaceCapabilityNameLabel]) > 0
+	getCrossplaneEnabledAnnotationName, err := getAnnotationEnvVarName("DFDS_CROSSPLANE_ENABLED_ANNOTATION_NAME", "dfds-crossplane-enabled")
+	if err != nil {
+		log.Log.Error(err, "unable to get Crossplane Enabled annotation name")
+	}
+
+	getAWSAccountIDAnnotationName, err := getAnnotationEnvVarName("DFDS_CROSSPLANE_AWS_ACCOUNT_ID_ANNOTATION_NAME", "dfds-aws-account-id")
+	if err != nil {
+		log.Log.Error(err, "unable to get AWS Account annotation name")
+	}
+
+	crossplaneEnabled := "false"
+	if len(namespace.Annotations[getCrossplaneEnabledAnnotationName]) > 0 {
+		log.Log.Info("Crossplane annotation value found: " + namespace.Annotations[getCrossplaneEnabledAnnotationName])
+		crossplaneEnabled = namespace.Annotations[getCrossplaneEnabledAnnotationName]
+	}
+
+	awsAccountId := ""
+	if len(namespace.Annotations[getAWSAccountIDAnnotationName]) > 0 {
+		log.Log.Info("AWS Account ID value found: " + namespace.Annotations[getAWSAccountIDAnnotationName])
+		awsAccountId = namespace.Annotations[getAWSAccountIDAnnotationName]
+	}
 
 	// TODO: Obtain this clusterrole info from a Configmap or other source, rather than hard code
 	clusterRole := &rbacv1.ClusterRole{
@@ -119,7 +137,7 @@ func (r *NamespaceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 	// TODO: We need to obtain this roleArn. Currently namespaces are not annotated with AWS Account ID
 	// so we may need to query the capability service to get them which is not ideal. It would be good
 	// if we could annotate namespaces on creation
-	roleArn := "something"
+	roleArn := awsAccountId
 
 	// TODO: Obtain this providerconfig info from a Configmap or other source, rather than hard code
 	providerAWS := &provideraws.ProviderConfig{
@@ -134,86 +152,120 @@ func (r *NamespaceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 		},
 	}
 
-	if labelIsPresent {
+	if crossplaneEnabled == "true" {
 
-		log.Log.Info("Capability detected on namespace " + namespace.Name)
-		controllerutil.SetControllerReference(&namespace, clusterRole, r.Scheme)
-		controllerutil.SetControllerReference(&namespace, clusterRoleBinding, r.Scheme)
-		controllerutil.SetControllerReference(&namespace, providerAWS, r.Scheme)
+		log.Log.Info("Detected Crossplane enabled on namespace " + namespace.Name)
 
-		// Create clusterrole if not exists
-		if err := r.Create(ctx, clusterRole); err != nil {
-			if apierrors.IsAlreadyExists(err) {
-				log.Log.Info("ClusterRole " + clusterRole.Name + " already exists for " + namespace.Name)
-
-				// Update clusterrole in case of changes
-				// TODO: Check if currently deployed clusterrole matches clusterrole to deploy before applying update
-				if err := r.Update(ctx, clusterRole); err != nil {
-					log.Log.Info("Unable to update role " + clusterRole.Name + " for " + namespace.Name)
-				} else {
-					log.Log.Info("ClusterRole " + clusterRole.Name + " for " + namespace.Name + " has been updated")
-				}
-				err = nil
-			}
-			if err != nil {
-				log.Log.Info("Unable to make ClusterRole " + clusterRole.Name + " for " + namespace.Name)
-				return ctrl.Result{}, err
-			}
+		if awsAccountId == "" {
+			log.Log.Info("No AWS Account ID found on namespace. Skipping creation of resources")
 		} else {
-			log.Log.Info("ClusterRole " + clusterRole.Name + " created for " + namespace.Name)
-		}
+			controllerutil.SetControllerReference(&namespace, clusterRole, r.Scheme)
+			controllerutil.SetControllerReference(&namespace, clusterRoleBinding, r.Scheme)
+			controllerutil.SetControllerReference(&namespace, providerAWS, r.Scheme)
 
-		// Create clusterrolebinding if not exists
-		if err := r.Create(ctx, clusterRoleBinding); err != nil {
-			if apierrors.IsAlreadyExists(err) {
-				log.Log.Info("ClusterRoleBinding " + clusterRoleBinding.Name + " already exists for " + namespace.Name)
+			// Create clusterrole if not exists
+			if err := r.Create(ctx, clusterRole); err != nil {
+				if apierrors.IsAlreadyExists(err) {
+					log.Log.Info("ClusterRole " + clusterRole.Name + " already exists for " + namespace.Name)
 
-				// Update clusterrolebinding in case of changes
-				// TODO: Check if currently deployed clusterrolebinding matches clusterrolebindnig to deploy before applying update
-				if err := r.Update(ctx, clusterRoleBinding); err != nil {
-					log.Log.Info("Unable to update clusterrolebinding " + clusterRoleBinding.Name + " for " + namespace.Name)
-				} else {
-					log.Log.Info("ClusterRoleBinding " + clusterRoleBinding.Name + " for " + namespace.Name + " has been updated")
+					// Update clusterrole in case of changes
+					// TODO: Check if currently deployed clusterrole matches clusterrole to deploy before applying update
+					if err := r.Update(ctx, clusterRole); err != nil {
+						log.Log.Info("Unable to update role " + clusterRole.Name + " for " + namespace.Name)
+					} else {
+						log.Log.Info("ClusterRole " + clusterRole.Name + " for " + namespace.Name + " has been updated")
+					}
+					err = nil
 				}
-				err = nil
-			}
-			if err != nil {
-				log.Log.Info("Unable to make ClusterRoleBinding " + clusterRoleBinding.Name + " for " + namespace.Name)
-				return ctrl.Result{}, err
-			}
-		} else {
-			log.Log.Info("ClusterRoleBinding " + clusterRoleBinding.Name + " created for " + namespace.Name)
-		}
-
-		// Create providerconfig if not exists
-		if err := r.Create(ctx, providerAWS); err != nil {
-			if apierrors.IsAlreadyExists(err) {
-				log.Log.Info("ProviderConfig " + providerAWS.Name + " already exists for " + namespace.Name)
-
-				// Update providerconfig in case of changes
-				// TODO: Check if currently deployed providerconfig matches providerconfig to deploy before applying update
-				if err := r.Update(ctx, providerAWS); err != nil {
-					log.Log.Info("Unable to update ProviderConfig " + providerAWS.Name + " for " + namespace.Name)
-				} else {
-					log.Log.Info("ProviderConfig " + providerAWS.Name + " for " + namespace.Name + " has been updated")
+				if err != nil {
+					log.Log.Info("Unable to make ClusterRole " + clusterRole.Name + " for " + namespace.Name)
+					return ctrl.Result{}, err
 				}
-				err = nil
+			} else {
+				log.Log.Info("ClusterRole " + clusterRole.Name + " created for " + namespace.Name)
 			}
 
-			//TODO: Check whether the ProviderConfig Kind exists in the cluster and silence error
+			// Create clusterrolebinding if not exists
+			if err := r.Create(ctx, clusterRoleBinding); err != nil {
+				if apierrors.IsAlreadyExists(err) {
+					log.Log.Info("ClusterRoleBinding " + clusterRoleBinding.Name + " already exists for " + namespace.Name)
 
-			if err != nil {
-				log.Log.Info("Unable to make ProviderConfig " + providerAWS.Name + " for " + namespace.Name)
-				return ctrl.Result{}, err
+					// Update clusterrolebinding in case of changes
+					// TODO: Check if currently deployed clusterrolebinding matches clusterrolebindnig to deploy before applying update
+					if err := r.Update(ctx, clusterRoleBinding); err != nil {
+						log.Log.Info("Unable to update clusterrolebinding " + clusterRoleBinding.Name + " for " + namespace.Name)
+					} else {
+						log.Log.Info("ClusterRoleBinding " + clusterRoleBinding.Name + " for " + namespace.Name + " has been updated")
+					}
+					err = nil
+				}
+				if err != nil {
+					log.Log.Info("Unable to make ClusterRoleBinding " + clusterRoleBinding.Name + " for " + namespace.Name)
+					return ctrl.Result{}, err
+				}
+			} else {
+				log.Log.Info("ClusterRoleBinding " + clusterRoleBinding.Name + " created for " + namespace.Name)
 			}
-		} else {
-			log.Log.Info("ProviderConfig " + providerAWS.Name + " created for " + namespace.Name)
+
+			// Create providerconfig if not exists
+			if err := r.Create(ctx, providerAWS); err != nil {
+				if apierrors.IsAlreadyExists(err) {
+					log.Log.Info("ProviderConfig " + providerAWS.Name + " already exists for " + namespace.Name)
+
+					// Update providerconfig in case of changes
+					/*
+						For some reason we need to retrieve the providerConfig and set the ResourceVersion on our referenced
+						object otherwise we get an error saying "ResourceVersion" must be specified for an update. Maybe there
+						is a better way to do it, but this seems to work for now.
+						TODO: Investigate better way of performing updte on ProviderConfig
+					*/
+
+					getProviderConfig := &provideraws.ProviderConfig{
+						ObjectMeta: metav1.ObjectMeta{
+							Name: "" + namespace.Name + "-aws",
+						},
+						Spec: provideraws.ProviderConfigSpec{
+							Credentials: provideraws.ProviderCredentials{
+								Source: "InjectedIdentity",
+							},
+							AssumeRoleARN: &roleArn,
+						},
+					}
+
+					if err := r.Get(ctx, client.ObjectKey{
+						Namespace: namespace.Name,
+						Name:      providerAWS.Name,
+					}, getProviderConfig); err != nil {
+						log.Log.Info("Unable to get ProviderConfig \n\n" + err.Error())
+					} else {
+						log.Log.Info("Successfully retrieved ProviderConfig. ResourceVersion: " + getProviderConfig.ResourceVersion)
+						providerAWS.SetResourceVersion(getProviderConfig.GetResourceVersion())
+					}
+
+					if err := r.Update(ctx, providerAWS); err != nil {
+						log.Log.Info("Unable to update providerconfig " + providerAWS.Name + " for " + providerAWS.Name + "\n\n" + err.Error())
+					} else {
+						log.Log.Info("ProviderConfig " + providerAWS.Name + " for " + namespace.Name + " has been updated")
+					}
+
+					err = nil
+				}
+
+				//TODO: Check whether the ProviderConfig Kind exists in the cluster and silence error
+
+				if err != nil {
+					log.Log.Info("Unable to make ProviderConfig " + providerAWS.Name + " for " + namespace.Name)
+					return ctrl.Result{}, err
+				}
+			} else {
+				log.Log.Info("ProviderConfig " + providerAWS.Name + " created for " + namespace.Name)
+			}
 		}
 
 		return ctrl.Result{}, nil
 
 	} else {
-		log.Log.Info("Capability not detected on namespace " + namespace.Name)
+		log.Log.Info("Detected Crossplane not enabled on namespace " + namespace.Name)
 
 		// Delete clusterrole if exists
 		if err := r.Delete(ctx, clusterRole); err != nil {
@@ -264,4 +316,13 @@ func (r *NamespaceReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&corev1.Namespace{}).
 		Complete(r)
+}
+
+func getAnnotationEnvVarName(envVarName string, defaultVarName string) (string, error) {
+	annotation, found := os.LookupEnv(envVarName)
+	if !found {
+		ctrl.Log.Info("No " + envVarName + " environment variable set. Using default of " + defaultVarName)
+		return defaultVarName, nil
+	}
+	return annotation, nil
 }
